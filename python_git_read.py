d4b34358b53ffd8893a05f19f9bfb6664e96aae5
Puneet Katyal <puneet.katyal@gds.ey.com>
2023-12-19 14:37:19 +0530

    updating git 19-12-23

---
+++ python_git_read.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+import io
+username= 'puneet664'
+token = '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'
+github_session = requests.Session()
+github_session.auth = (username, token)
+
+# url = 'https://github.com/puneet664/test_dbt_qw/blob/main/data_test.csv'
+url='https://raw.githubusercontent.com/puneet664/test_dbt_qw/main/data_test.csv'
+export = requests.get(url,verify=False).text
+print(export)
+df = pd.read_csv(io.StringIO(export))
+print(df)
