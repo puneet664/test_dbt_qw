cfded64f73315fbf5ac3241cfd8b8a380bf5346f
puneet664 <151123084+puneet664@users.noreply.github.com>
2023-12-26 14:02:33 +0530

    Update python_git_read.py

--- python_git_read.py
+++ python_git_read.py
@@ -3,43 +3,22 @@ import requests
 import io
 from io import StringIO
 from git import Repo
-username= 'puneet664'
-token = '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'
-github_session = requests.Session()
-github_session.auth = (username, token)
+#username= 'puneet664'
+#token = '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'
+#github_session = requests.Session()
+#github_session.auth = (username, token)
 
 # url = 'https://github.com/puneet664/test_dbt_qw/blob/main/data_test.csv'
-url='https://raw.githubusercontent.com/puneet664/test_dbt_qw/main/data_test.csv'
-export = requests.get(url,verify=False).text
-df = pd.read_csv(io.StringIO(export))
+#url='https://raw.githubusercontent.com/puneet664/test_dbt_qw/main/data_test.csv'
+#export = requests.get(url,verify=False).text
+#df = pd.read_excel(io.StringIO(export))
+xls=pd.ExcelFile('Data Taxonomy_workforce.xlsx')
+df = pd.read_excel(xls, 'LO ')
 # Convert DataFrame to CSV in memory
-csv_buffer = StringIO()
-df.to_csv(csv_buffer, index=False)
-csv_content = csv_buffer.getvalue()
- 
-# Path to your local repository
-repo_path = 'https://github.com/puneet664/test_dbt_qw.git'  # Replace with your local repository path
- 
-# Commit message
-commit_message = 'Add CSV file'
- 
-# File name for the CSV in the repository
-file_name = 'data_push.csv'
- 
-# Open the Git repository
-repo = Repo()
-print(repo)
-# Write the CSV content to a file in the repository
-with open(f'{file_name}', 'w') as file:
-    file.write(csv_content)
- 
-# Stage changes
-repo.index.add([file_name])
- 
-# Commit changes
-repo.index.commit(commit_message)
- 
-# Push changes to remote (optional)
-origin = repo.remote(name='origin')  # Replace 'origin' with your remote name
-origin.push()
-# df.to_csv("file_name_git.csv")
+#csv_buffer = StringIO()
+#df.to_csv(csv_buffer, index=False)
+#csv_content = csv_buffer.getvalue()
+final_json = df.to_json(orient='records')[1:-1].replace('},{', '} {')
+with open('business_glossary_code_workforce.yaml', 'w') as outfile:
+    yaml.dump(final_json, outfile, sort_keys=False, default_flow_style=False)
+   
